2399c5ef61bc734cd8e19e88b9e5f783d0481c51
Sven Cerk <sven.cerk@gmail.com>
2018-07-24 17:03:51 +0200

    fix #includes

--- main.cpp
+++ main.cpp
@@ -3,52 +3,52 @@
 #include <fstream>
 #include <iostream>
 
-#include "include/read_gal.h"
-#include "include/read_amalfi.h"
-#include "include/read_ldgraphs.h"
+#include <sics/read_gal.h>
+#include <sics/read_amalfi.h>
+#include <sics/read_ldgraphs.h>
 
-#include "include/adjacency_listmat.h"
+#include <sics/adjacency_listmat.h>
 
-#include "include/vertex_order.h"
+#include <sics/vertex_order.h>
 
-#include "include/stats.h"
+#include <sics/stats.h>
 
-#include "include/backtracking_ind.h"
-#include "include/backtracking_degreeprune_ind.h"
-#include "include/backtracking_adjacentconsistency_ind.h"
-#include "include/backtracking_degreeprune_adjacentconsistency_ind.h"
-#include "include/backtracking_adjacentconsistency_forwardcount_ind.h"
-#include "include/backtracking_degreeprune_adjacentconsistency_forwardcount_ind.h"
-#include "include/backtracking_forwardcount_ind.h"
-#include "include/backtracking_parent_ind.h"
-#include "include/backtracking_parent_degreeprune_ind.h"
-#include "include/backtracking_parent_adjacentconsistency_ind.h"
-#include "include/backtracking_parent_degreeprune_adjacentconsistency_ind.h"
-#include "include/backtracking_parent_forwardcount_ind.h"
-#include "include/backtracking_parent_adjacentconsistency_forwardcount_ind.h"
-#include "include/backtracking_parent_degreeprune_adjacentconsistency_forwardcount_ind.h"
-#include "include/backtracking_adjacentconsistency_precount_ind.h"
-#include "include/backtracking_degreeprune_adjacentconsistency_precount_ind.h"
-#include "include/backtracking_parent_adjacentconsistency_precount_ind.h"
-#include "include/backtracking_parent_degreeprune_adjacentconsistency_precount_ind.h"
-#include "include/backjumping_ind.h"
-#include "include/conflictbackjumping_ind.h"
-#include "include/conflictbackjumping_degreeprune_ind.h"
-#include "include/backmarking_ind.h"
-#include "include/backmarking_degreeprune_ind.h"
-#include "include/forwardchecking_ind.h"
-#include "include/forwardchecking_degreeprune_ind.h"
-#include "include/lazyforwardchecking_ind.h"
-#include "include/lazyforwardchecking_degreeprune_ind.h"
-#include "include/lazyforwardchecking_low_ind.h"
-#include "include/lazyforwardchecking_low_degreeprune_ind.h"
-#include "include/lazyforwardchecking_parent_ind.h"
-#include "include/lazyforwardchecking_parent_degreeprune_ind.h"
-#include "include/lazyforwardchecking_low_parent_ind.h"
-#include "include/lazyforwardchecking_low_parent_degreeprune_ind.h"
+#include <sics/backtracking_ind.h>
+#include <sics/backtracking_degreeprune_ind.h>
+#include <sics/backtracking_adjacentconsistency_ind.h>
+#include <sics/backtracking_degreeprune_adjacentconsistency_ind.h>
+#include <sics/backtracking_adjacentconsistency_forwardcount_ind.h>
+#include <sics/backtracking_degreeprune_adjacentconsistency_forwardcount_ind.h>
+#include <sics/backtracking_forwardcount_ind.h>
+#include <sics/backtracking_parent_ind.h>
+#include <sics/backtracking_parent_degreeprune_ind.h>
+#include <sics/backtracking_parent_adjacentconsistency_ind.h>
+#include <sics/backtracking_parent_degreeprune_adjacentconsistency_ind.h>
+#include <sics/backtracking_parent_forwardcount_ind.h>
+#include <sics/backtracking_parent_adjacentconsistency_forwardcount_ind.h>
+#include <sics/backtracking_parent_degreeprune_adjacentconsistency_forwardcount_ind.h>
+#include <sics/backtracking_adjacentconsistency_precount_ind.h>
+#include <sics/backtracking_degreeprune_adjacentconsistency_precount_ind.h>
+#include <sics/backtracking_parent_adjacentconsistency_precount_ind.h>
+#include <sics/backtracking_parent_degreeprune_adjacentconsistency_precount_ind.h>
+#include <sics/backjumping_ind.h>
+#include <sics/conflictbackjumping_ind.h>
+#include <sics/conflictbackjumping_degreeprune_ind.h>
+#include <sics/backmarking_ind.h>
+#include <sics/backmarking_degreeprune_ind.h>
+#include <sics/forwardchecking_ind.h>
+#include <sics/forwardchecking_degreeprune_ind.h>
+#include <sics/lazyforwardchecking_ind.h>
+#include <sics/lazyforwardchecking_degreeprune_ind.h>
+#include <sics/lazyforwardchecking_low_ind.h>
+#include <sics/lazyforwardchecking_low_degreeprune_ind.h>
+#include <sics/lazyforwardchecking_parent_ind.h>
+#include <sics/lazyforwardchecking_parent_degreeprune_ind.h>
+#include <sics/lazyforwardchecking_low_parent_ind.h>
+#include <sics/lazyforwardchecking_low_parent_degreeprune_ind.h>
 
-#include "include/forwardchecking_mrv_degreeprune_ind.h"
-#include "include/forwardchecking_mrv_degreeprune_refine_ind.h"
+#include <sics/forwardchecking_mrv_degreeprune_ind.h>
+#include <sics/forwardchecking_mrv_degreeprune_refine_ind.h>
 
 int main(int argc, char * argv[]) {
   using namespace sics;
@@ -57,16 +57,16 @@ int main(int argc, char * argv[]) {
   char const * h_filename = argv[2];
 
   std::ifstream in{g_filename,std::ios::in|std::ios::binary};
-  auto g = read_amalfi<adjacency_listmat<uint16_t, bidirectional_tag>>(in);
+  auto g = read_gal<adjacency_listmat<uint16_t, undirected_tag>>(in);
   in.close();
   in.open(h_filename,std::ios::in|std::ios::binary);
-  auto h = read_amalfi<adjacency_listmat<uint16_t, bidirectional_tag>>(in);
+  auto h = read_gal<adjacency_listmat<uint16_t, undirected_tag>>(in);
   in.close();
 
   auto index_order_g = vertex_order_GreatestConstraintFirst(g);
 
   int count = 0;
-  lazyforwardchecking_low_parent_degreeprune_ind(
+  backmarking_degreeprune_ind(
       g,
       h,
       [&count]() {++count; return true;},
